cfe714a15550ce12411785293bbef59011adb74d
cezarszl <cezarszlfedora@gmail.com>
2025-05-16 19:44:57 +0200

    fix: update import statement for qrCodeBase64 to include file extension

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { qrCodeBase64 } from './whatsapp';
+import { qrCodeBase64 } from './whatsapp.js';
 
 const app = express();
 const port = process.env.BACKEND_PORT || 3000;
