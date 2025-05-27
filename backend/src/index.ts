c29f9011d7b2708d0ed6c2abf79c43cdcf4ff3c3
cezarszl <cezarszlfedora@gmail.com>
2025-05-27 06:37:04 +0200

    fix: update backend server configuration to use API_BASE_URL and default port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,19 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import './whatsapp';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const baseUrl = process.env.API_BASE_URL;
 
 const app = express();
-const port = process.env.BACKEND_PORT || 3000;
+const port = process.env.BACKEND_PORT;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', routes);
 
-app.get('/ping', (req, res) => {
-    res.json({ message: 'pong' });
-});
 
 app.listen(port, () => {
-    console.log(`🚀 Backend running at http://localhost:${port}`);
+    console.log(`🚀 Backend running at ${baseUrl}:${port}`);
 });
