5c72e043d7d84ea85ec89ce8ba457c3ffef63751
cezarszl <cezarszlfedora@gmail.com>
2025-05-27 07:09:02 +0200

    fix: update WhatsAppLogin component to handle QR code loading states and improve error handling

--- frontend/src/components/WhatsAppLogin.vue
+++ frontend/src/components/WhatsAppLogin.vue
@@ -1,27 +1,32 @@
 <template>
 
-<div v-if="qr">
-    <img :src="qr" alt="Scan the QR code to log in" />
-</div>
 
-<p v-else>Loading QR code...</p>
+<img v-if="typeof qr === 'string'" :src="qr" alt="Scan the QR code to log in" />
+
+<p v-else-if="qr === null">Ładowanie kodu QR...</p>
+<p v-else-if="qr === false">Już zalogowany ✅</p>
 
 </template>
 
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-const qr = ref<string| null>(null);
+const qr = ref<string | boolean | null>(null);
 
 onMounted(async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/whatsapp/auth/qr');
-    const data = await res.json();
-    console.log('QR:', data);
-    qr.value = data.qr;
+    const res = await fetch(`${baseUrl}/api/whatsapp/auth/qr`);
+    if (res.ok) {
+      const data = await res.json();
+      qr.value = data.qr;
+    } else {
+      qr.value = false; 
+    }
   } catch (e) {
     console.error('❌ Fetch QR failed', e);
+    qr.value = false;
   }
 });
 
