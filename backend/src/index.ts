c0c3407f0b100ca48c2530739fa61f060959c323
cezarszl <cezarszlfedora@gmail.com>
2025-06-27 19:18:35 +0200

    refactor: reorganize imports and enhance signal message receiving logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,23 +2,36 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import './whatsapp';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import { setupWebSocket } from './ws';
 import { createServer } from 'http';
+import { receiveMessages } from './signal/service';
+
 dotenv.config();
 
 const baseUrl = process.env.API_BASE_URL;
+const port = process.env.BACKEND_PORT;
+const MY_NUMBER = process.env.SIGNAL_PHONE_NUMBER!;
+
+export const SIGNAL_NUMBER = MY_NUMBER;
 
 const app = express();
-const port = process.env.BACKEND_PORT;
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/api', routes);
 
 const server = createServer(app);
 setupWebSocket(server);
+
 server.listen(port, () => {
     console.log(`🚀 Backend running at ${baseUrl}:${port}`);
 });
+
+setInterval(async () => {
+    try {
+        await receiveMessages(SIGNAL_NUMBER);
+    } catch (err) {
+        console.error('❌ Error in periodic Signal receive:', err);
+    }
+}, 5000);
